01cdeb3388dcf930db2d1bf47f7973159889ba9c
Ariana <u202211917@upc.edu.pe>
2025-11-11 01:35:57 -0500

    Commit de arreglo Home Component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, signal } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from './Componente/home-component/home-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('OjoCiudadano-FRONTEND');
-  public nombre: string = "Ojo Ciudadano";
 }
