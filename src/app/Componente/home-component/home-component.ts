5d0c129f3a87c62328c2fcee5266c6d76f75b555
Joseph Flores <josephfloresvillaverde@gmail.com>
2025-11-11 16:27:56 -0500

    Actualizo frontend con los últimos cambios

--- src/app/Componente/home-component/home-component.ts
+++ src/app/Componente/home-component/home-component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+// src/app/home-component/home-component.ts
+import { Component, OnInit } from '@angular/core';
+import {CommonModule, NgClass} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home-component',
-  imports: [],
   templateUrl: './home-component.html',
-  styleUrl: './home-component.css',
+  imports: [
+    NgClass,
+    CommonModule,
+    RouterLink
+  ],
+  styleUrls: ['./home-component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  // Variable para controlar el estado del menú
+  isMenuOpen: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  // Metodo para alternar el estado del menú
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  logout() {
+
+  }
 }
+
