5abf2c71b1c643b0c3d41030bc04787354b9da07
Joseph Flores <josephfloresvillaverde@gmail.com>
2025-11-14 19:38:40 -0500

    WIP: Guardando progreso antes de actualizar con dev

--- src/app/Componente/panel-evidencia-component/panel-evidencia-component.ts
+++ src/app/Componente/panel-evidencia-component/panel-evidencia-component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-panel-evidencia', // Nombre del selector
   templateUrl: './panel-evidencia-component.html',
   styleUrls: ['./panel-evidencia-component.css'],
-  imports: [CommonModule]
+  imports: [CommonModule, RouterLink]
 })
 export class PanelEvidenciaComponent implements OnInit { // <-- CLASE EXPORTADA Y CONFIGURADA
 
